b7b90e046ec885cf4e8c1eb0f2c03c01d1eecca8
Anmol Git <Anmol>
2023-05-08 16:02:24 +0530

    sidebar done

--- Front End/my-app/src/Redux/productReducer/action.js
+++ Front End/my-app/src/Redux/productReducer/action.js
@@ -1,15 +1,33 @@
-import axios from "axios"
-import { PRODUCTFAILURE, PRODUCTREQUEST, PRODUCTSUCCESS } from "./actionType"
+import axios from "axios";
+import { PRODUCTFAILURE, PRODUCTREQUEST, PRODUCTSUCCESS } from "./actionType";
 
-
-
-export const getVitamins =(obj)=> (dispatch)=>{
-    dispatch(PRODUCTREQUEST)
-    axios.get("https://good-pear-cheetah-tutu.cyclic.app/vitamin",obj).then((res)=>{
-        dispatch({type:PRODUCTSUCCESS,payload:res.data})
-        console.log(res)
-    }).catch((err)=>{
-        dispatch(PRODUCTFAILURE)
-        console.log(err)
+export const getVitamins = (obj) => (dispatch) => {
+  dispatch(PRODUCTREQUEST);
+  axios
+    .get("https://good-pear-cheetah-tutu.cyclic.app/vitamin", obj)
+    .then((res) => {
+      dispatch({ type: PRODUCTSUCCESS, payload: res.data });
+      console.log(res.data);
     })
-}
+    .catch((err) => {
+      dispatch(PRODUCTFAILURE);
+      console.log(err);
+    });
+};
+
+export const getVitaminsside = (obj) => (dispatch) => {
+  dispatch(PRODUCTREQUEST);
+//   console.log(obj);
+  axios
+    .get("https://good-pear-cheetah-tutu.cyclic.app/vitamin", {
+      params: obj,
+    })
+    .then((res) => {
+      dispatch({ type: PRODUCTSUCCESS, payload: res.data });
+      console.log(res.data);
+    })
+    .catch((err) => {
+      dispatch(PRODUCTFAILURE);
+      console.log(err);
+    });
+};
