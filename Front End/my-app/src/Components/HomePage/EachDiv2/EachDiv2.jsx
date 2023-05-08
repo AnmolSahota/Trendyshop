bbcfc2d995959488581bf59fba984e49942b6dd7
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-05-08 11:37:36 +0530

    lll

---
+++ Front End/my-app/src/Components/HomePage/EachDiv2/EachDiv2.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import styles from "./EachDiv2.module.css";
+
+const EachDiv2 = () => {
+  return (
+    <div className={styles.eachDiv}>
+      <div>
+        <div>
+          <p className={styles.caption}>Price drop on Headphones</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/273f3eaccb62700c.jpeg?q=50"
+            alt="kkk"
+            // style={{width:'100%',height:'100%'}}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Exciting offers with card</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/af1679c7c77321c0.jpg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Most affordable mobiles</p>
+          <img
+            src="	https://rukminim1.flixcart.com/fk-p-flap/480/480/image/0e5d1a3a3d52c6c1.jpg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Sale on smart watches</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/ec5ede30554a288c.jpeg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Sale on Electronics</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/05320031e7644896.jpg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Big savings on Wooden items</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/0258b470ce5ea080.jpg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Summer Sale is live</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/1a89e2868b9b20c6.jpg?q=50"
+            alt=""
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className={styles.caption}>Drink pure save pure</p>
+          <img
+            src="https://rukminim1.flixcart.com/fk-p-flap/480/480/image/ee90e283965b0109.jpeg?q=50"
+            alt="image1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EachDiv2;
